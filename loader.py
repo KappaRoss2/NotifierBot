e49867707eae27d63bad8fe62d032890868bf8e2
KappaRoss <vam.vam.10@mail.ru>
2022-07-27 19:50:13 +0500

    add some comments

--- loader.py
+++ loader.py
@@ -2,5 +2,6 @@ from aiogram import Bot, Dispatcher
 import config
 
 
+# Инициализируем бота и диспетчер
 bot = Bot(token=config.token)
 dp = Dispatcher(bot)
--- main.py
+++ main.py
@@ -5,5 +5,6 @@ from handlers.general import process_info_command
 from loader import dp
 
 
+# Скрипт для запуска бота
 if __name__ == '__main__':
     executor.start_polling(dp)
