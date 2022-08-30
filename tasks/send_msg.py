9cecc7b9f870679ad6333b563e0f4d97db510430
KappaRoss <vam.vam.10@mail.ru>
2022-08-30 23:47:24 +0500

    send_msg fix

--- tasks/send_msg.py
+++ tasks/send_msg.py
@@ -65,7 +65,7 @@ async def send_msg_serial():
 # Запускаем функции send_msg_anime и send_msg_serial с интервалом в один день.
 scheduler = AsyncIOScheduler()
 
-scheduler.add_job(send_msg_anime, "cron", hour=18, minute=45)
+scheduler.add_job(send_msg_anime, "cron", hour=12, minute=0)
 scheduler.add_job(send_msg_serial, "cron", hour=12, minute=0)
 
 scheduler.start()
